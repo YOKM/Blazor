0a3aaabbfcb1a15ba786fd6fd64e2aa2ed3f45f5
Ahmad <cparwez@gmail.com>
2018-09-26 12:57:12 +0100

    Update KOFAXContext.cs

--- ImagingTaskSchedule/ImagingTaskSchedule.Shared/Models/KOFAXContext.cs
+++ ImagingTaskSchedule/ImagingTaskSchedule.Shared/Models/KOFAXContext.cs
@@ -14,7 +14,6 @@ namespace ImagingTaskSchedule.Shared.Models {
         protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
             if (!optionsBuilder.IsConfigured) {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server= ukcsd01rap035\\SQLExpress;Database=KOFAX;user id=KOFAX;password=KOFAX;Trusted_Connection=True;MultipleActiveResultSets=true");
              
             }
         }
