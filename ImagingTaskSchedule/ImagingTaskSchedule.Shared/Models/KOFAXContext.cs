1606dd82c0067fdb33ad29c4d519d9a03b7ed1e4
Gaston <cparwez@gmail.com>
2018-09-26 11:34:46 +0100

    Context

--- ImagingTaskSchedule/ImagingTaskSchedule.Shared/Models/KOFAXContext.cs
+++ ImagingTaskSchedule/ImagingTaskSchedule.Shared/Models/KOFAXContext.cs
@@ -15,7 +15,6 @@ namespace ImagingTaskSchedule.Shared.Models {
             if (!optionsBuilder.IsConfigured) {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer ("Server= TEST\\SQLExpress;Database=TEST;user id=TEST;password=TEST;Trusted_Connection=True;MultipleActiveResultSets=true");
-
             }
         }
 
